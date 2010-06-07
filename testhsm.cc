47f6bb1b3453c320574eb98b5620d47726aeed79
Russell Steicke <russells@adelie.cx>
2010-06-07 09:35:26 +0800

    Add licence text.  Beerware!
    Signed-off-by: Russell Steicke <russells@adelie.cx>

--- testhsm.cc
+++ testhsm.cc
@@ -1,3 +1,12 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENCE" (Revision 42):
+ * <russells@adelie.cx> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. Russell Steicke.
+ * ----------------------------------------------------------------------------
+ */
+
 #include "testhsm.hh"
 
 int main(int argc, char **argv)
